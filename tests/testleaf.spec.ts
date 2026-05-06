b6be4ad3be3b3e3b36822a3b51088ba5d1b79014
DhivyaRajagoundan <dhivya1136@yahoo.com>
2026-05-06 20:20:17 +0530

    commit

--- tests/testleaf.spec.ts
+++ tests/testleaf.spec.ts
@@ -6,7 +6,5 @@ await page.goto("https://login.salesforce.com/?locale.in");
 await page.locator('#username').fill('dilipkumar.rajendran@testleaf.com')
 await page.locator('input[id="password"]').fill('TestLeaf@2025');
 await page.locator('#Login').click();
-await page.waitForTimeout(12000);
-await page.locator('[title="App Launcher"]').click()
 
 })
