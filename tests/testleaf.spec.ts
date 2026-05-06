b11edad0c2755fac249ac420c89e6e32a0a2a6af
DhivyaRajagoundan <dhivya1136@yahoo.com>
2026-05-06 21:00:31 +0530

    my shwwwmt

--- tests/testleaf.spec.ts
+++ tests/testleaf.spec.ts
@@ -6,5 +6,7 @@ await page.goto("https://login.salesforce.com/?locale.in");
 await page.locator('#username').fill('dilipkumar.rajendran@testleaf.com')
 await page.locator('input[id="password"]').fill('TestLeaf@2025');
 await page.locator('#Login').click();
+console.log("commiting to GIT");
+
 
 })
